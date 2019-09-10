cf30c47e3ba46cf5502c0036919e029251266d35
Sathish <Sathish@DESKTOP-UM71UTQ>
2019-09-10 23:34:08 +0530

    Code changes

---
+++ src/main/java/assessment/retail_store/exceptions/RetailStoreDiscountException.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package assessment.retail_store.exceptions;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import assessment.retail_store.BillProducer;
+
+/**
+ * @author Sathish
+ *
+ */
+public class RetailStoreDiscountException extends Exception {
+
+	private static final Logger LOGGER = LogManager.getLogger(RetailStoreDiscountException.class);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RetailStoreDiscountException(String message) {
+		super(message);
+		LOGGER.error("{}", message);
+	}
+
+}
