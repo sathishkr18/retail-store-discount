50acceb0e44f741b2c5801f00a8b3f4dcb773ed4
Sathish <Sathish@DESKTOP-UM71UTQ.mshome.net>
2019-09-11 00:22:39 +0530

    Code changes

--- src/main/java/assessment/retail_store/RetailStoreApp.java
+++ src/main/java/assessment/retail_store/RetailStoreApp.java
@@ -12,6 +12,7 @@ import assessment.retail_store.discount.AffiliateDiscounter;
 import assessment.retail_store.discount.CustomerDiscounter;
 import assessment.retail_store.discount.Discounter;
 import assessment.retail_store.discount.EmployeeDiscounter;
+import assessment.retail_store.enums.UserType;
 import assessment.retail_store.exceptions.RetailStoreDiscountException;
 
 /**
@@ -32,22 +33,18 @@ public class RetailStoreApp {
 			BillProducer app = new BillProducer();
 			Discounter zeroPercentDiscounter = amount -> amount;
 
-			switch (user.getType()) {
-			case EMPLOYEE:
+			if(UserType.EMPLOYEE.equals(user.getType())){
 				return app.calculateNetPayableAmount(items, new EmployeeDiscounter(), user.getUserInfo());
-			case AFFILIATE:
+			}else if(UserType.AFFILIATE.equals(user.getType())) {
 				return app.calculateNetPayableAmount(items, new AffiliateDiscounter(), user.getUserInfo());
-			case CUSTOMER:
-				if (user.getFromDate().isBefore(LocalDate.now().minusYears(2))) {
+			}else if(UserType.CUSTOMER.equals(user.getType()) && user.getFromDate().isBefore(LocalDate.now().minusYears(2))) {
 					return app.calculateNetPayableAmount(items, new CustomerDiscounter(), user.getUserInfo());
-				}
-				return app.calculateNetPayableAmount(items, zeroPercentDiscounter, user.getUserInfo());
-			}
+			} 
+			return app.calculateNetPayableAmount(items, zeroPercentDiscounter, user.getUserInfo());				
 
 		} catch (Exception e) {
 			throw new RetailStoreDiscountException("Unexpected error occurred");
 		}
-		return null;
 	}
 
 }
